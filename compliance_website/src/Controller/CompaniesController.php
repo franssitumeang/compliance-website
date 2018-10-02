4c925a675fe7a0215397957a9dba911e4fff428c
F <framelah1@gmail.com>
2018-10-02 08:17:18 +0700

    update company crud

--- compliance_website/src/Controller/CompaniesController.php
+++ compliance_website/src/Controller/CompaniesController.php
@@ -5,36 +5,54 @@ use App\Controller\AppController;
 use Cake\Event\Event;
 
 class CompaniesController extends AppController{
+
+    public $paginate = [
+        'limit' => 10,
+        'order' => [
+            'Companies.company_name' => 'asc'
+        ]
+    ];
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadComponent('Paginator');
+    }
+
     public function beforeFilter(Event $event){
         $this->viewBuilder()->layout('admin');
     }
+
     public function index()
     {
         $title = "Perusahaan";
         $this->set('title', $title);
-        
-        $companies = $this->Companies->find('all');
+        $companies = $this->paginate('Companies');
         $company = $this->Companies->newEntity();
-        $this->set(compact('companies','company'));
-        $this->set([
-            '_serialize' => ['companies','company']
-            ]);
-            
-            $this->viewBuilder()->templatePath('Admins');
-            $this->render('company');
+        $paginate = $this->Paginator->getPagingParams()["Companies"];
+        $this->set(compact('companies','company','paginate'));
+        
+        $this->viewBuilder()->templatePath('Admins');
+        $this->render('company');
+    }
+    
+    
+    public function add($id = null){
+        if($id == null){
+            $company = $this->Companies->newEntity();
+        }else{
+            $company = $this->Companies->get($id);
         }
-        public function add(){
-            if ($this->request->is('post')) {
-                $company = $this->Companies->newEntity();
-                $company = $this->Companies->patchEntity($company, $this->request->getData());
-                if ($this->Companies->save($company)) {
-                    $this->Flash->set(__('The company has been saved.'));
-                }else{
-                    // $this->Flash->set(__('The company could not be saved. Please, try again.'));
-                    $this->Flash->set(__($this->Companies->validationErrors));
-                }
-                return $this->redirect(['action' => 'index']);
+        if ($this->request->is('post')) {
+            $company = $this->Companies->patchEntity($company, $this->request->getData());
+            if ($this->Companies->save($company)) {
+                $message = 'The company has been saved.';
+                
+            }else{
+                $message = 'The company could not be saved. Please, try again.';
             }
-            
+            $this->Flash->set(__($message));
+            return $this->redirect(['action' => 'index']);
         }
     }
+}
